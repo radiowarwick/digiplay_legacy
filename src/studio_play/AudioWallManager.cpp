6ca27e448bde5e5ed557e6c01af6ad708e79e5a6
ccantwell <ccantwell@radio.warwick.ac.uk>
2006-07-16 23:01:15 +0000

    Defined AudioWallItem structure in the AudioWallManager class Added array operator to AudioWallManager class
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@82 156110d5-7a05-0410-a37f-c1950ce4f093

--- src/studio_play/AudioWallManager.cpp
+++ src/studio_play/AudioWallManager.cpp
@@ -1,5 +1,3 @@
-#include <string>
-#include <vector>
 #include <iostream>
 using namespace std;
 
@@ -13,6 +11,28 @@ AudioWallManager::~AudioWallManager() {
 
 }
 
-void AudioWallManager::setUser( string username ) {
+AudioWallItem& AudioWallManager::operator[] (unsigned int index) {
+	try {
+		return at(index);
+	}
+	catch (int e) {
+		throw e;
+	}
+}
+
+AudioWallItem& AudioWallManager::at(unsigned int index) {
+	if (index < _items.size()) {
+		return _items.at(index);
+	}
+	else {
+		throw AUDIOWALL_INDEX_OUT_OF_BOUNDS;
+	}
+}
+
+void AudioWallManager::setUser( QString username ) {
 	_username = username;
+}
+
+void AudioWallManager::changeWall( unsigned int index ) {
+
 }
--- src/studio_play/AudioWallManager.h
+++ src/studio_play/AudioWallManager.h
@@ -1,19 +1,40 @@
 #ifndef CLASS_AUDIO_WALL_MANAGER
 #define CLASS_AUDIO_WALL_MANAGER
 
+#include <vector>
+using namespace std;
+
 #include "pqxx/connection.h"
 #include "pqxx/transaction.h"
 #include "pqxx/result.h"
 using namespace pqxx;
 
-class AudioWallManager {
+#include <qobject.h>
+#include <qstring.h>
+#include <qfont.h>
+#include <qcolor.h>
+
+#define AUDIOWALL_INDEX_OUT_OF_BOUNDS 12000
+
+struct AudioWallItem {
+	QString text;
+	QFont font;
+	QColor color;
+};
+
+class AudioWallManager : public QObject {
+	Q_OBJECT
 	public:
 		AudioWallManager( Connection *C );
 		~AudioWallManager();
-		void setUser( string username );
+		AudioWallItem& operator[] (unsigned int index);
+		AudioWallItem& at(unsigned int index);
+		void setUser( QString username );
+		void changeWall( unsigned int index );
 
 	private:
-		string _username;
+		QString _username;
+		vector<AudioWallItem> _items;
 };
 
 #endif

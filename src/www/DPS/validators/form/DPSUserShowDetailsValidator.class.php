72d777f776e56214fd2c173e116751367769d0d5
ChrisJHawley <chawley@radio.warwick.ac.uk>
2007-06-06 23:36:02 +0000

    Reformatted and changed @package.
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@209 156110d5-7a05-0410-a37f-c1950ce4f093

--- src/www/DPS/validators/form/DPSUserShowDetailsValidator.class.php
+++ src/www/DPS/validators/form/DPSUserShowDetailsValidator.class.php
@@ -1,18 +1,17 @@
 <?php
 /**
- *
- * @package FrontEnds
- * @subpackage Auth
- */
+*
+* @package DPS
+*/
 
 include_once($cfg['MVC']['dir']['root'] . '/MVCUtils.class.php');
 MVCUtils::includeValidator('ValidatorRule', 'MVC');
 MVCUtils::includeFormValidator('FormValidator', 'tkfecomon');
 /**
- * Check that a string is a valid name
- *
- *
- */
+* Check that a string is a valid name
+*
+*
+*/
 class DPSUserShowDetailsValidator extends FormValidator {
 
 	//protected $fieldData;
@@ -25,11 +24,10 @@ class DPSUserShowDetailsValidator extends FormValidator {
 		global $cfg;
 		
 		$db = Database::getInstance($cfg['MVC']['dsn']);
-		$rules = $db->getAll("SELECT vrclassname, description, fieldname, fieldvalidators.modulename FROM
-		                      fieldvalidators, formfields WHERE
-		                      formfields.ruleid = fieldvalidators.ruleid AND
-		                      
-		                      formname = '{$this->formName}'");
+		$rules = $db->getAll("SELECT vrclassname, description, fieldname, 
+			fieldvalidators.modulename FROM fieldvalidators, formfields 
+			WHERE formfields.ruleid = fieldvalidators.ruleid
+			AND formname = '{$this->formName}'");
 		//This statement has been removed from the where clause:
 		//modulename = '{$this->fieldData['moduleName']}' AND
 		$invalidFields = array();
@@ -69,7 +67,7 @@ class DPSUserShowDetailsValidator extends FormValidator {
 	private function checkDir($dirID, $check) {
 		global $cfg;
 		$db = Database::getInstance($cfg['DPS']['dsn']);
-		$sql = "select id from dir where parent = " . pg_escape_string($dirID);
+		$sql = "SELECT id FROM dir WHERE parent = " . pg_escape_string($dirID);
 		$subDirs = $db->getColumn($sql);
 		if(in_array($check,$subDirs)) {
 			return true;
@@ -82,7 +80,5 @@ class DPSUserShowDetailsValidator extends FormValidator {
 		}
 		return false;
 	}
-
 }
-
 ?>

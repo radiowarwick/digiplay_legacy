738e9c4ce4a41f686d78e8a745b17f012f198d73
Simon Pain <spain@radio.warwick.ac.uk>
2007-06-08 11:16:47 +0000

    Made sue log and get player device location from config
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@213 156110d5-7a05-0410-a37f-c1950ce4f093

--- src/sueplay/sueplay.cpp
+++ src/sueplay/sueplay.cpp
@@ -40,6 +40,8 @@ using namespace pqxx;
 using namespace Audio;
 
 #include "Config.h"
+#include "dps.h"
+#include "Logger.h"
 
 #define min(a,b) (((a)<(b))?(a):(b))
 
@@ -53,7 +55,7 @@ int main(int argc, char *argv) {
 	Result R;
 	
 	cout << " -> Reading configuration file" << endl;
-    Config *Conf = new Config("digiplay");
+	Config *Conf = new Config("digiplay");
 
 	cout << " -> Connecting to Database..." << flush;
 	Connection *C;
@@ -66,7 +68,7 @@ int main(int argc, char *argv) {
 		cout << "Failed to connect to database" << endl;
 		exit(-1);
 	}
-	SQL_Item = "SELECT archives.localpath AS path, audio.md5 AS md5, audio.title AS title, audio.length_smpl AS length_smpl, sustschedule.id AS id, sustschedule.trim_start_smpl AS start, sustschedule.trim_end_smpl AS end, sustschedule.fade_in AS fade_in, sustschedule.fade_out AS fade_out FROM sustschedule, audio, archives WHERE sustschedule.audioid = audio.id AND archives.id = audio.archive ORDER BY sustschedule.id LIMIT 1";
+	SQL_Item = "SELECT archives.localpath AS path, audio.md5 AS md5, audio.title AS title, audio.length_smpl AS length_smpl, sustschedule.id AS id, sustschedule.trim_start_smpl AS start, sustschedule.trim_end_smpl AS end, sustschedule.fade_in AS fade_in, sustschedule.fade_out AS fade_out, v_audio_track.artist FROM sustschedule, audio, archives, v_audio_track WHERE sustschedule.audioid = audio.id AND archives.id = audio.archive AND audio.id = v_audio_track.id ORDER BY sustschedule.id LIMIT 1";
 	cout << "done." << endl;
 
 	// Create components
@@ -74,7 +76,7 @@ int main(int argc, char *argv) {
 	ProcessFader* fader[] = {new ProcessFader(), new ProcessFader()};
 	CounterTrigger* trig[] = {new CounterTrigger(), new CounterTrigger()};
 	ProcessMixer* mixer = new ProcessMixer();
-	OutputDsp* player = new OutputDsp("/dev/dsp");
+	OutputDsp* player = new OutputDsp(Conf->getParam("channel_1").c_str());
 
 	ch[0]->connect(OUT0,fader[0],IN0);
 	ch[1]->connect(OUT0,fader[1],IN0);
@@ -87,7 +89,7 @@ int main(int argc, char *argv) {
 	ch[0]->addCounter(trig[0]);
 	ch[1]->addCounter(trig[1]);
 
-	string md5, path;
+	string md5, path, title, artist;
 	long start = 0, end = 0, fade_in = 0, fade_out = 0;
 	long length_smpl = 0;
 	unsigned short active = 0, inactive = 1;
@@ -115,6 +117,8 @@ int main(int argc, char *argv) {
 			// Load the required data from database
 			path = R[0]["path"].c_str();
 			md5 = R[0]["md5"].c_str();
+			title = R[0]["title"].c_str();
+			artist = R[0]["artist"].c_str();
 			start = atoi(R[0]["start"].c_str());
 			end = atoi(R[0]["end"].c_str());
 			fade_in = atoi(R[0]["fade_in"].c_str());
@@ -133,15 +137,17 @@ int main(int argc, char *argv) {
 			
 			path += "/" + md5.substr(0,1) + "/";
 			cout << "Attempting to load channel " << active << ": " 
-					<< R[0]["title"].c_str() << endl;
+					<< artist << " - " << title << endl;
 			cout << " -> Start: " << start << endl;
 			cout << " -> End: " << end << endl;
 			// Try and load the track
 			try {
+				cout << "Loading " << path << md5 << endl;
 				ch[active]->load( path + md5, start, end );
 				break;
 			}
 			catch (...) {
+				cout << "Error loading track" << endl;
 			}
 			sleep(1);
 		} while (1);
@@ -186,6 +192,26 @@ int main(int argc, char *argv) {
 			cout << "Playing channel " << active << endl << endl;
 			ch[active]->play();
 		}
+
+		cout << " -------> Logging " << artist << " - " << title << endl;
+		char *routine = "sueplay::main";
+		int now = (int)time(NULL);
+		artist = sqlesc(artist);
+		title = sqlesc(title);
+		T = new Transaction(*C,"");
+		string SQL_Insert = "INSERT INTO log "
+			"(userid, datetime, track_title, track_artist, location) "
+			"VALUES (" + dps_itoa(1) + ", " + dps_itoa(now) + ", '"
+			+ title + "', '" + artist + "', " + dps_itoa(0) + ");";
+		try {
+			T->exec(SQL_Insert);
+			T->commit();
+		}
+		catch (...) {
+			cout << "Failed to log record" << endl;
+			L_ERROR(LOG_TABLOGGING,"Failed to log record " + artist + " - " + title + ".");
+		}
+			delete T;
 		
 		active = abs(active - 1);
 		inactive = abs(inactive - 1);

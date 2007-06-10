56c93f0e223388a0d3f601522fd3547a0c8d4d50
ccantwell <ccantwell@radio.warwick.ac.uk>
2007-06-10 15:07:11 +0000

    Added defines for default database values
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@226 156110d5-7a05-0410-a37f-c1950ce4f093

--- src/DbDefine.h
+++ src/DbDefine.h
@@ -1,3 +1,19 @@
-#define GROUP_EVERYONE "1"
+#define USER_SYSTEM "1"
+#define USER_GUEST "2"
 
+#define GROUP_EVERYONE "1"
+#define GROUP_ADMINISTRATORS "2"
+
+#define DIR_DIGIPLAY "1"
+#define DIR_MUSIC "2"
+#define DIR_BIN "3"
+#define DIR_ADVERTS "4"
+#define DIR_DEPARTMENTS "5"
+#define DIR_JINGLES "6"
 #define DIR_USERS "7"
+
+#define ALBUM_NONE "1"
+
+#define AUDIO_TYPE_MUSIC "1"
+#define AUDIO_TYPE_JINGLE "2"
+#define AUDIO_TYPE_ADVERT "3"

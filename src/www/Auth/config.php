52134406565bd0c500c0008d760cdd4f9b0e18ee
ccantwell <ccantwell@radio.warwick.ac.uk>
2007-08-18 11:51:04 +0000

    Tidied up configuration of site All configuration values which might need to be changed can be changed in the top level config.php file, overriding default settings for each module.
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@303 156110d5-7a05-0410-a37f-c1950ce4f093

---
+++ src/www/Auth/config.php
@@ -0,0 +1,68 @@
+<?php
+
+######################
+## Auth Config
+######################
+
+/*
+ * These are the DSNs for each module. If a value is set to null 
+ * then the default value will be used ($cfg['DBAL']['dsn']), 
+ * and therefore it must be set.
+ * 
+ * For information on DSNs please see: 
+ * http://pear.php.net/manual/en/package.database.db.intro-dsn.php
+ *
+ */
+$cfg['Auth']['dsn']     = null;
+$cfg['Auth']['LDAPDS']  = "ldapserver";
+$cfg['Auth']['LDAPsr']  = "ou=People,dc=example,dc=com";
+
+//The path to the Auth module
+$cfg['Auth']['dir']['root'] = $cfg['general']['toolkitRoot'] . '/Auth';
+
+//The maximum idle time before a user is logged out (in seconds)q
+$cfg['Auth']['lifetime'] = 1800;
+
+//The name of the template to be used for displaying permission errors. This is either a file name
+//or a template id. This must not contain sensitive information as it is 
+//not always secured
+$cfg['Auth']['permissionErrorTemplate'] = 'dpspermissionError.tpl';
+
+//The module in which the "permissionErrorTemplate" resides
+$cfg['Auth']['permissionErrorTemplateModule'] = 'tkfecommon';
+
+//The name of the template to be used for displaying permission errors. This is either a file name
+//or a template id. This must not contain sensitive information as it is 
+//not always secured
+$cfg['Auth']['rendererPermissionErrorTemplate'] = 'rPermissionError.tpl';
+
+//It is possible to add columns to the Auth tables to store extra data.
+//The arrays below store a list of these extra fields that you wish to be
+//editable in the front end for the Auth module. The key is the field name,
+//the value is a user friendly name.       
+
+//The anonymous user for the site. This user account is what non-loggedin users
+//browse as.
+$cfg['Auth']['anonuser'] = 'Guest';
+$cfg['Auth']['anonuserID'] = 2;
+//redirect user to login if they fail auth and are logged in as the guest account (y/n)
+$cfg['Auth']['anonuserredirect'] = 'y';
+//template to redirect users from above too
+$cfg['Auth']['anonuserRedirectTemplateID'] = 11;
+//Editable fields in the users table
+$cfg['Auth']['userfields'] = array('username' => 'Username');
+
+//Editable fields in the groups table
+$cfg['Auth']['groupfields'] = array('name' => 'Group Name',
+                                   'description' => 'Description');
+
+//Editable fields in the realms table
+$cfg['Auth']['realmfields'] = array('name' => 'Realm Name',
+                                    'description' => 'Description');   
+
+$cfg['Auth']['authClassModule'] = 'Auth';
+$cfg['Auth']['authUtilClassModule'] = 'AuthUtil';
+
+$cfg['Auth']['defaultNewUserGroup'] = 1;
+$cfg['Auth']['AdminGroup'] = 2;
+?>
---
+++ src/www/CMS/config.php
@@ -0,0 +1,47 @@
+<?php
+
+######################
+## CMS Config
+######################
+
+/*
+ * These are the DSNs for each module. If a value is set to null 
+ * then the default value will be used ($cfg['DBAL']['dsn']), 
+ * and therefore it must be set.
+ * 
+ * For information on DSNs please see: 
+ * http://pear.php.net/manual/en/package.database.db.intro-dsn.php
+ *
+ */
+$cfg['CMS']['dsn'] = null;
+
+//The path to the CMS module
+$cfg['CMS']['dir']['root'] = $cfg['general']['toolkitRoot'] . '/CMS';
+
+//The path to the FCKeditor directory
+$cfg['CMS']['fckPath'] = $cfg['general']['toolkitRoot'] . '/FCKeditor';
+
+//The URL of the FCKEditor directory
+$cfg['CMS']['fckURL'] = $cfg['general']['toolkitRootURL'] . '/FCKeditor'; 
+
+//Custom config file url
+$cfg['CMS']['configURL'] = $cfg['general']['toolkitRootURL'] . '/CMS/cmsconfig.js'; #CHECK
+
+	//You can overide the above setting, and specify a 'configURL' in a 
+	//different module by using this setting. If for example, you specify 
+	//TKWiki here, then the $cfg['TKWiki']['configURL'] setting will be used 
+	//instead of $cfg['CMS']['configURL']. By default, this should be 
+	//left as 'CMS'
+	$cfg['CMS']['configURLModule'] = 'CMS';
+
+//Toolbars which can be assumed to be present in the FCKeditor config file.
+//By default, the config file is cmsconfig.js
+$cfg['CMS']['availableToolbars'] = array('cms_full', 'cms_safe', 'cms_simple', 'cms_none');
+
+//If true, this option will always show the editor if the user has permission to edit.
+//If set to false, the editor can be shown by passing edit=1 in the query string
+$cfg['CMS']['showEditorByDefault'] = true;
+
+//The height of the editor
+$cfg['CMS']['editorHeight'] = 400;
+?>

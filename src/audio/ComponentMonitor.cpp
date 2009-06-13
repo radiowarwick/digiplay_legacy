a4f251583a5adc1f0bb9d7303ef78100e8a25737
ccantwell <ccantwell@radio.warwick.ac.uk>
2009-06-13 16:53:00 +0000

    Actually add the new classes.
    git-svn-id: https://www.radio.warwick.ac.uk/repos/dps/trunk@581 156110d5-7a05-0410-a37f-c1950ce4f093

---
+++ src/audio/ComponentMonitor.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+#include "ComponentMonitor.h"
+using Audio::ComponentMonitor;
+
+/**
+ * Creates a new component. This should not be called directly.
+ */
+ComponentMonitor::ComponentMonitor() {
+
+}
+
+ComponentMonitor::~ComponentMonitor() {
+    
+}

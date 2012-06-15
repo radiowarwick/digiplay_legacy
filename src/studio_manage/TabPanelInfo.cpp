a47982482a8d3d5e8b3ca16650f2a1d97447bcf5
jonty-comp <jontysewell@gmail.com>
2012-06-16 00:41:04 +0100

    Fixed tab description

--- src/studio_manage/TabPanelInfo.cpp
+++ src/studio_manage/TabPanelInfo.cpp
@@ -80,7 +80,7 @@ void TabPanelInfo::draw() {
 		"<P>On the touch screen are three 'Audio Players' which correspond to three channels on the Broadcast Desk. These function just like a normal CD player.</P>\n"
 		"<P>There is also an audio wall that contains jingles and beds that you can play without searching the library. To start them, just press their button, and it will come out of the fourth channel on the desk. In the future the bottom wall will be fully customisable, while the top wall will contain the station jingles, adverts and promotions.<p>\n"
 		"<H3>Library Search</H3>\n"
-		"<P>The third tab above is an Audio Library search engine. It allows you to find music in the audio library to add to your playlist. Enter criteria for the track you are looking for and click search. Double click on a track to add it to the bottom of the show plan.</P>\n"
+		"<P>The second tab above is an Audio Library search engine. It allows you to find music in the audio library to add to your playlist. Enter criteria for the track you are looking for and click search. Double click on a track to add it to the bottom of the show plan.</P>\n"
 		"<H3>Playing Music out</H3>\n"
 		"<P>Click the load button on any of the three players to load the top track in the show plan. Use the play \\ stop \\ pause buttons like a normal CD player. You can jump through the track by dragging the position bar when in pause or stop mode. Click the time button to toggle between the Elapsed and Remaining time display.\n"
 		"<H3>Problems or Suggestions?</H3>\n"
